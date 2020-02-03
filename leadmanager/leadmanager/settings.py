274f100938d41347e36aa1b7f8298c8eced36d3d
TareqMonwer <tareqmonwer137@gmail.com>
2020-02-04 02:21:37 +0600

    leads api ready

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # local
+    'leads',
+    
+    # 3rd party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ leadmanager/leads/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+
+from .api import LeadViewset
+
+
+router = routers.DefaultRouter()
+router.register('api/leads', LeadViewset, 'leads')
+
+
+urlpatterns = router.urls
